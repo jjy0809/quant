b1f5fcba6b6f47a076b781629a640ce2f0ba21fa
jjy0809 <117441526+jjy0809@users.noreply.github.com>
2025-07-21 02:28:42 +0900

    Add files via upload

---
+++ new_quant.py
@@ -0,0 +1,42 @@
+import finterstellar as fs
+import matplotlib.pyplot as plt
+
+graph_path = r"C:\Users\happy\Desktop\학교\고등학교\2학년\LSTM과 뉴스 헤드라인 감성분석을 통한 주식 예측\graphs\quant"
+
+#symbols = ['MU', 'XLI', 'LCID', 'GS', 'INTC', 'F', 'PYPL', 'BAC', 'GDX', 'EWY', 'PEP', 'KO', 'GOOGL']
+#symbols = ['UBER', 'TSLA', 'PINS', 'RBLX', 'SBUX', 'DIS', 'WMT', 'QCOM', 'IBM', 'SPOT', 'ADBE']
+symbols = ['META', 'MMM', 'AMZN', 'NKE', 'MSFT', 'BA', 'AZN', 'MCD', 'NVDA', 'AMD', 'CPNG'] # 종목 리스트
+cost = 0.01 # 매매 수수료
+rf_rate = 0.028 # 무위험 이자율
+
+print("\n_______________________________________\n")
+
+for symbol in symbols: # 각 종목에 대해 백테스팅
+    df = fs.get_price(symbol, start_date='2025-01-01', end_date='2025-07-18') # 날짜 범위에 대한 종목의 주가 가져오기
+    #print(df.tail())
+    
+    hp = df[symbol].max() # 최고가
+    lp = df[symbol].min() # 최저가
+    dif_ratio = (hp - lp) / lp # 최고가와 최저가 차이 비율
+    s = 0.033 + 0.015 * dif_ratio # w값 종목별로 유동적으로 수정
+    fs.envelope(df, w=30, spread=s) # 엔벨로프 계산
+    #print(df.tail())
+    #fs.draw_band_chart(df) # 엔벨로프 그래프 생성
+    #plt.title(f"{symbol} Envelpe Graph (s = {s})")
+    #plt.savefig(graph_path + '/' +  f"{symbol}_envelpoe_{s:.4f}.png") # 그래프 저장
+    #print(f"Saving {symbol}_{s:.4f} Succesfully")
+    #plt.show()
+    
+    print(f"{symbol} 모멘텀 전략 결과:")
+    fs.band_to_signal(df, buy='A', sell='B') # 모멘텀 시그널
+    fs.position(df)
+    fs.evaluate(df, cost=cost)
+    fs.performance(df, rf_rate=rf_rate) # 백테스팅
+    print("\n_______________________________________\n")
+    
+    print(f"{symbol} 평균회귀 전략 결과:")
+    fs.band_to_signal(df, buy='D', sell='B') # 평균회귀 시그널
+    fs.position(df)
+    fs.evaluate(df, cost=cost)
+    fs.performance(df, rf_rate=rf_rate) # 백테스팅
+    print("\n_______________________________________\n")
